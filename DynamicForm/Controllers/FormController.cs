500a41b6c723ab1189866003027590e243156744
Musa Funso Oyedele <56215827+mfoyedele@users.noreply.github.com>
2024-05-14 09:38:25 +0100

    initial commit

---
+++ DynamicForm/Controllers/FormController.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace ApplicationFormAPI.Controllers
+{
+    [Route("api/questions")]
+    [ApiController]
+    public class FormController : ControllerBase
+    {
+        
+    }
+}

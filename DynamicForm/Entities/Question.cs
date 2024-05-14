4d745b46d599731e105e0030d31a0de862c1962e
Musa Funso Oyedele <56215827+mfoyedele@users.noreply.github.com>
2024-05-14 15:57:34 +0100

    Questions and questionType entities added

---
+++ DynamicForm/Entities/Question.cs
@@ -0,0 +1,27 @@
+﻿namespace DynamicForm.Entities
+{
+    public abstract class Question
+    {
+        public int Id { get; }
+        public string Type { get; }
+        public string Text { get; set; }
+
+        protected Question(string type, string text)
+        {
+            Id = GenerateId();
+            Type = type;
+            Text = text;
+        }
+
+        protected Question(string text)
+        {
+            Text = text;
+        }
+
+        private static int GenerateId()
+        {
+
+            return Guid.NewGuid().GetHashCode();
+        }
+    }
+}
---
+++ DynamicForm/Entities/QuestionTypes.cs
@@ -0,0 +1,45 @@
+﻿namespace DynamicForm.Entities
+{
+    public class QuestionTypes
+    {
+        public class ParagraphQuestion : Question
+        {
+            public ParagraphQuestion(string text) : base("paragraph", text) { }
+        }
+
+        public class YesNoQuestion : Question
+        {
+            public YesNoQuestion(string text) : base(text) { }
+        }
+
+        public class DropdownQuestion : Question
+        {
+            public List<string> Options { get; set; }
+
+            public DropdownQuestion(string text, List<string> options) : base("dropdown", text)
+            {
+                Options = options;
+            }
+        }
+
+        public class MultipleChoiceQuestion : Question
+        {
+            public List<string> Options { get; set; }
+
+            public MultipleChoiceQuestion(string text, List<string> options) : base(text)
+            {
+                Options = options;
+            }
+        }
+
+        public class DateQuestion : Question
+        {
+            public DateQuestion(string text) : base(text) { }
+        }
+
+        public class NumberQuestion : Question
+        {
+            public NumberQuestion(string text) : base(text) { }
+        }
+    }
+}

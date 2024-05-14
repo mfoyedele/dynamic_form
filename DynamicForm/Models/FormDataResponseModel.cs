b77ad5a1e031a60a99cfdbd6167e9e1e064129f7
Musa Funso Oyedele <56215827+mfoyedele@users.noreply.github.com>
2024-05-14 10:29:14 +0100

    Input and response models added for form

---
+++ DynamicForm/Models/FormDataResponseModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DynamicForm.Models
+{
+    public class FormDataResponseModel
+    {
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? Email { get; set; }
+        public int PhoneNumber { get; set; }
+        public string? Nationality { get; set; }
+        public int IdNumber { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime DateOfBirth { get; set; }
+        public string? Gender { get; set; }
+        public List<QuestionInputModel> Questions { get; set; }
+    }
+}
---
+++ DynamicForm/Models/QuestionResponseModel.cs
@@ -0,0 +1,10 @@
+﻿namespace DynamicForm.Models
+{
+    public class QuestionResponseModel
+    {
+        public int Id { get; set; }
+        public string Type { get; set; }
+        public string Text { get; set; }
+        public List<string> Options { get; set; }
+    }
+}
